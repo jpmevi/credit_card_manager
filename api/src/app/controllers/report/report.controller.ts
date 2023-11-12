c8affa138d4045c100cf2a04a9075f2806769f58
AlexanderLuther <helmuthalexander-luthermontejo@cunoc.edu.gt>
2023-11-12 00:39:58 -0600

    Reporte de detalle de cuenta agregado

--- api/src/app/controllers/report/report.controller.ts
+++ api/src/app/controllers/report/report.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Account } from 'src/app/entities/Account.entity';
 import { ReportService } from 'src/app/services/report/report.service';
 
 @Controller('report')
@@ -7,13 +8,22 @@ export class ReportController {
     constructor(private reportService : ReportService) { }
 
 
+    /**
+     * This endpoint returns a list of accounts and group them by status.
+     * @returns [{amount: number, status: string}]
+     */
     @Get('/accountsReport')
     async getAccountsReport() : Promise<any[]> {
         return this.reportService.getAccountsReport();
     }
 
+    /**
+     * This endpoint returns the account and details that matches with number received as parameter.
+     * @param number 
+     * @returns 
+     */
     @Get('/accountDetailReport/:number')
-    async getAccountDetailReport(@Param() number : string) : Promise<any> {
+    async getAccountDetailReport(@Param('number') number) : Promise<Account> {
         return this.reportService.getAccountDetailReport(number);
     }
 
--- api/src/app/services/report/report.service.ts
+++ api/src/app/services/report/report.service.ts
@@ -6,23 +6,31 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class ReportService {
 
-    constructor(@InjectRepository(Account) private accountRepository : Repository<Account> ) { }
+    constructor(@InjectRepository(Account) private reportRepository : Repository<Account> ) { }
 
     /**
      * This method counts the number of accounts and group them by status.
      * @returns [{amount: number, status: string}]
      */
     async getAccountsReport() : Promise<any[]>{
-       return await this.accountRepository.createQueryBuilder("account")
+       return await this.reportRepository.createQueryBuilder("account")
         .select(`COUNT(status)`, "amount")
         .addSelect("status", "status")
         .addGroupBy("status")
         .getRawMany();
     }
 
-    async getAccountDetailReport(number : string) : Promise<any> {
-        var account =  await this.accountRepository.findOne({where :{ number: number }}).then(account => account);
-        if(account == null) throw new NotFoundException('Account with that Credit card number not found');
+    /**
+     * This method returns the account and details that matches with number received as parameter.
+     * @param number 
+     * @returns 
+     */
+    async getAccountDetailReport(number : string) : Promise<Account> {
+        var account =  await this.reportRepository.findOne({
+            relations: ['user'],
+            where :{ number: number }    
+        }).then(account => account);
+        if (account == null) throw new NotFoundException('Account with that Credit card number not found');
         return account;
     }
 
