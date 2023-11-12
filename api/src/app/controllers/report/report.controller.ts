24d0085ffdc7e637b98eb073de1a4775edd8f8f3
potzmevi <juanmezavi@gmail.com>
2023-11-12 04:37:28 -0600

    added al movement reports

--- api/src/app/controllers/report/report.controller.ts
+++ api/src/app/controllers/report/report.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Account } from 'src/app/entities/Account.entity';
+import { Transaction } from 'src/app/entities/Transaction.entity';
 import { ReportService } from 'src/app/services/report/report.service';
-
+@ApiTags('Report')
 @Controller('report')
 export class ReportController {
 
-    constructor(private reportService : ReportService) { }
+    constructor(private reportService: ReportService) { }
 
 
     /**
@@ -13,7 +15,7 @@ export class ReportController {
      * @returns [{amount: number, status: string}]
      */
     @Get('/accountsReport')
-    async getAccountsReport() : Promise<any[]> {
+    async getAccountsReport(): Promise<any[]> {
         return this.reportService.getAccountsReport();
     }
 
@@ -23,8 +25,38 @@ export class ReportController {
      * @returns 
      */
     @Get('/accountDetailReport/:number')
-    async getAccountDetailReport(@Param('number') number) : Promise<Account> {
+    async getAccountDetailReport(@Param('number') number): Promise<Account> {
         return this.reportService.getAccountDetailReport(number);
+    }
+
+    /**
+    * This endpoint returns a list of all account movements in the api.
+    * @returns 
+    */
+    @Get('/movementsReport')
+    @ApiResponse({
+        status: 200,
+        description: 'Success. Returns a list of movements.',
+    })
+    async getMovementsReport(): Promise<any[]> {
+        return this.reportService.getMovementsReport();
+    }
+
+    /**
+    * This endpoint returns a list of movements of one account by number.
+    * @returns 
+    */
+    @ApiResponse({
+        status: 200,
+        description: 'Success. Returns a list of movements.',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Not Found. No movements found for the provided account number.',
+    })
+    @Get('/movementsReport/:number')
+    async getMovementsDetailReport(@Param('number') number): Promise<any[]> {
+        return this.reportService.getMovementsDetailReport(number);
     }
 
 }
